e4ea8106a06f0c743052e75c9c1ab849fcd2bd4e
Kaiden Munsey <kaidenmnsey@aol.com>
2020-05-01 19:35:58 -0400

    <adding JS variables and function outlines>

---
+++ assets/javascript/app.js
@@ -0,0 +1,82 @@
+$(document).ready(function (){
+    let correctAnswers = 0;
+    let inCorrectAnswers = 0;
+    let rightAnswer; 
+    let unAnseredQuestions = 0;
+    let timeLeft = 20;
+    let indexTrivia = 0; //index of all in triviaQuesions array
+    let triviaQuestions = [{ //trivia questions 
+        question: "",
+        answers: [""],
+        rightAnswer: "",
+        image: "",
+    }, {
+        question: "",
+        answers: [""],
+        rightAnswer: "",
+        image: "",
+    }, {
+        question: "",
+        answers: [""],
+        rightAnswer: "",
+        image: "",
+    },{
+        question: "",
+        answers: [""],
+        rightAnswer: "",
+        image: "",
+    },{
+        question: "",
+        answers: [""],
+        rightAnswer: "",
+        image: "",
+    },{
+        question: "",
+        answers: [""],
+        rightAnswer: "",
+        image: "",
+    },{
+        question: "",
+        answers: [""],
+        rightAnswer: "",
+        image: "",
+    },{
+        question: "",
+        answers: [""],
+        rightAnswer: "",
+        image: "",
+    }];
+
+
+    function answeredCorrect () { //function for when correct answer is chosen 
+
+    }
+
+    function answeredIncorrect () { //function when incorrect answer is chosen 
+
+    }
+
+
+    function startGame () { //function to start game when user clicks button 
+
+    }
+
+    function timer () { //countdown timer for each question
+
+    }
+
+    function nextQuestion () { //function to call next question with no user input 
+
+    }
+
+    function reset () { //function to reset game after time runs out or all questions answered 
+        
+    }
+
+
+
+
+
+
+
+})
